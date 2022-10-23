f07149873ca4843c4e79746c9777eb8305dbdcca
William Chalk <3halk1@gmail.com>
2022-10-22 22:42:01 -0400

    added voting system

--- models/index.js
+++ models/index.js
@@ -1,7 +1,12 @@
 const User = require("./User");
 const Post = require("./Post");
+const Vote = require("./Vote");
 
 User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Vote, {
   foreignKey: "user_id",
 });
 
@@ -9,4 +14,28 @@ Post.belongsTo(User, {
   foreignKey: "user_id",
 });
 
-module.exports = { User, Post };
+Post.belongsToMany(User, {
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "post_id",
+});
+
+Post.hasMany(Vote, {
+  foreignKey: "post_id",
+});
+
+Vote.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Vote.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+Vote.belongsToMany(Post, {
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "user_id",
+});
+
+module.exports = { User, Post, Vote };
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,12 +1,24 @@
 const router = require("express").Router();
 const { json } = require("body-parser");
-const { Post, User } = require("../../models");
+const { Post, User, Vote } = require("../../models");
+const sequelize = require("../../config/connection");
 
 //get all users posts
 router.get("/", (req, res) => {
   console.log("==============");
   Post.findAll({
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          `(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)`
+        ),
+        "vote_count",
+      ],
+    ],
     order: [["created_at", "DESC"]],
     include: [
       {
@@ -27,7 +39,18 @@ router.get("/:id", (req, res) => {
     where: {
       id: req.params.id,
     },
-    attributes: ["id", "post_url", "title", "created_at"],
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          `(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)`
+        ),
+        "vote_count",
+      ],
+    ],
     include: [
       {
         model: User,
@@ -58,6 +81,15 @@ router.post("/", (req, res) => {
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
+    });
+});
+
+router.put("/upvote", (req, res) => {
+  Post.upvote(req.body, { Vote })
+    .then((updatedPostData) => res.json(updatedPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
     });
 });
 
